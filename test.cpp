caf2cfad2a598ea75946556915ab0a183ae96ad5
yakov7podkova <romannakova@yandex.com>
2023-03-01 00:19:55 +0300

    added and tested node constuctors and some node methods, added parent field to node class

--- test.cpp
+++ test.cpp
@@ -1,4 +1,17 @@
-#include "map"
-int main(){
+#include<string>
+#include "tree.h"
 
+using namespace yeet;
+
+int main() {
+    node<int> negr(0);
+    node<int> negr1(0);
+
+    node<int> negr2(static_cast<node<int>&&>(negr));
+
+    node<int> n(1, &negr, &negr1);
+
+
+    std::cout << n.leftptr() << ' ' << n.rightptr() << '\n';
+    std::cout << negr.leftptr() << ' ' << negr.rightptr();
 }
--- tree.h
+++ tree.h
@@ -1,7 +1,60 @@
-#pragma once 
+#pragma once
+#include "tree"
 #include <functional>
 #include <iostream> 
 
-int main() {
+template<typename val, typename cmp>
+yeet::node<val, cmp>::node() {
+    this->right = nullptr;
+    this->left = nullptr;
+    this->parent = nullptr;
+};
 
-}
+template<typename val, typename cmp>
+yeet::node<val, cmp>::node(val value, node *left, node *right, node *parent) {
+    this->value = value;
+    this->right = right;
+    this->left = left;
+    this->parent = parent;
+};
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>::node(const node &other) {
+    this->value = other.value;
+    this->right = other.right;
+    this->left = other.left;
+    this->parent = other.parent;
+};
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>::node(node &&other) {
+    this->value = other.value;
+    this->right = other.right;
+    this->left = other.left;
+    this->parent = other.parent;
+    other.right = nullptr;
+    other.left = nullptr;
+    other.parent = nullptr;
+};
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>::~node() {};
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>& yeet::node<val, cmp>::operator=(const node &other) {};
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>& yeet::node<val, cmp>::operator=(node &&other) {};
+
+template<typename val, typename cmp>
+val yeet::node<val, cmp>::get_value() {return this->value;}
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>* yeet::node<val, cmp>::leftptr() {return this->left;}
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>* yeet::node<val, cmp>::rightptr() {return this->right;}
+
+template<typename val, typename cmp>
+yeet::node<val, cmp>* yeet::node<val, cmp>::parentptr() {return this->parent;}
+

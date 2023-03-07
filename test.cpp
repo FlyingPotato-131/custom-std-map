a9d87b52d6f7440ddc13dedae34f5650c94bd91f
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2023-03-07 20:35:28 +0300

    finished nodes and added rotation

--- test.cpp
+++ test.cpp
@@ -1,17 +1,32 @@
+#include <iostream>
 #include<string>
 #include "tree.h"
 
 using namespace yeet;
 
 int main() {
-    node<int> negr(0);
-    node<int> negr1(0);
+    // node<int> negr(0);
+    // node<int> negr1(0);
 
-    node<int> negr2(static_cast<node<int>&&>(negr));
+    // node<int> negr2(static_cast<node<int>&&>(negr));
 
-    node<int> n(1, &negr, &negr1);
+    // node<int> n(1, &negr, &negr1);
 
 
-    std::cout << n.leftptr() << ' ' << n.rightptr() << '\n';
-    std::cout << negr.leftptr() << ' ' << negr.rightptr();
+    // std::cout << n.leftptr() << ' ' << n.rightptr() << '\n';
+    // std::cout << negr.leftptr() << ' ' << negr.rightptr();
+    node<char> C('C');
+    node<char> B('B', &C);
+    node<char> A('A', &B);
+    B.setparent(&A);
+    C.setparent(&B);
+    node<char> b('b', nullptr, nullptr, &B);
+    node<char> a('a', nullptr, nullptr, &A);
+    B.setright(&b);
+    A.setright(&a);
+    tree<char> Ta = &A;
+    tree<char> Tb = &B;
+    std::cout << Ta;
+    A.rightrot();
+    std::cout << Tb;
 }
--- tree.h
+++ tree.h
@@ -16,6 +16,15 @@ yeet::node<val, cmp>::node(val value, node *left, node *right, node *parent) {
     this->right = right;
     this->left = left;
     this->parent = parent;
+    if(left != nullptr && right != nullptr){
+        this->height = std::max(left->height, right->height) + 1;
+    }else if(right != nullptr){
+        this->height = right->height + 1;
+    }else if(left != nullptr){
+        this->height = left->height + 1;
+    }else{
+        this->height = 1;
+    }
 };
 
 template<typename val, typename cmp>
@@ -24,6 +33,7 @@ yeet::node<val, cmp>::node(const node &other) {
     this->right = other.right;
     this->left = other.left;
     this->parent = other.parent;
+    this->height = other.height;
 };
 
 template<typename val, typename cmp>
@@ -32,22 +42,37 @@ yeet::node<val, cmp>::node(node &&other) {
     this->right = other.right;
     this->left = other.left;
     this->parent = other.parent;
+    this->height = other.height;
     other.right = nullptr;
     other.left = nullptr;
     other.parent = nullptr;
 };
 
 template<typename val, typename cmp>
-yeet::node<val, cmp>::~node() {};
+yeet::node<val, cmp>::~node() {
+    // if(right != nullptr){
+    //     right->~node();
+    // }
+    // if(left != nullptr){
+    //     left->~node();
+    // }
+    // std::free(this);
+};
+
+// template<typename val, typename cmp>
+// yeet::node<val, cmp> &yeet::node<val, cmp>::operator=(const node &other) {
+//     left = other.left;
+//     right = other.right;
+//     value = other.value;
+//     parent = other.parent;
+//     height = other.height;
+// };
+
+// template<typename val, typename cmp>
+// yeet::node<val, cmp>& yeet::node<val, cmp>::operator=(node &&other) {};
 
 template<typename val, typename cmp>
-yeet::node<val, cmp>& yeet::node<val, cmp>::operator=(const node &other) {};
-
-template<typename val, typename cmp>
-yeet::node<val, cmp>& yeet::node<val, cmp>::operator=(node &&other) {};
-
-template<typename val, typename cmp>
-val yeet::node<val, cmp>::get_value() {return this->value;}
+val yeet::node<val, cmp>::getvalue() {return this->value;}
 
 template<typename val, typename cmp>
 yeet::node<val, cmp>* yeet::node<val, cmp>::leftptr() {return this->left;}
@@ -58,3 +83,110 @@ yeet::node<val, cmp>* yeet::node<val, cmp>::rightptr() {return this->right;}
 template<typename val, typename cmp>
 yeet::node<val, cmp>* yeet::node<val, cmp>::parentptr() {return this->parent;}
 
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::setvalue(val v) {value = v;}
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::setleft(yeet::node<val, cmp>* l) {left = l;}
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::setright(yeet::node<val, cmp>* r) {right = r;}
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::setparent(yeet::node<val, cmp>* p) {parent = p;}
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::updateheight(){
+     if(left != nullptr && right != nullptr){
+        this->height = std::max(left->height, right->height) + 1;
+    }else if(right != nullptr){
+        this->height = right->height + 1;
+    }else if(left != nullptr){
+        this->height = left->height + 1;
+    }else{
+        this->height = 1;
+    }
+}
+
+// template<typename val, typename cmp>
+// yeet::tree<val, cmp>::tree() {head = nullptr;}
+
+// template<typename val, typename cmp>
+// yeet::tree<val, cmp>::tree(node<val, cmp> nd) {head = &nd;}
+
+// template<typename val, typename cmp>
+// yeet::tree<val, cmp>::tree(const tree &other){
+//    head = other.head; 
+// }
+
+// template<typename val, typename cmp>
+// yeet::tree<val, cmp>::tree(tree &&other){
+//     head = other.head;
+//     other.head = nullptr;
+// }
+
+// template<typename val, typename cmp>
+// yeet::tree<val, cmp>::~tree() {
+//     // head->yeet::node<val, cmp>::~node();
+// }
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::leftrot(){
+    yeet::node<val, cmp> *A = this;
+    yeet::node<val, cmp> *B = this->right;
+    yeet::node<val, cmp> *C = this->right->right;
+    A->setright(B->leftptr());
+    if(A->rightptr() != nullptr){
+        A->right->setparent(A); 
+    }
+    B->setparent(A->parentptr());
+    A->setparent(B);
+    B->setleft(A);
+    if(B->parent != nullptr){
+        B->parent->setright(B);
+    }
+    A->updateheight();
+    B->updateheight();
+    C->updateheight();
+}
+
+template<typename val, typename cmp>
+void yeet::node<val, cmp>::rightrot(){
+    yeet::node<val, cmp> *A = this;
+    yeet::node<val, cmp> *B = this->left;
+    yeet::node<val, cmp> *C = this->left->left;
+    A->setleft(B->rightptr());
+    if(A->leftptr() != nullptr){
+        A->left->setparent(A); 
+    }
+    B->setparent(A->parentptr());
+    A->setparent(B);
+    B->setright(A);
+    if(B->parent != nullptr){
+        B->parent->setleft(B);
+    }
+    A->updateheight();
+    B->updateheight();
+    C->updateheight();
+}
+
+template<typename val, typename cmp = std::less<val>>
+using tree = yeet::node<val, cmp> *;
+
+template<typename val, typename cmp>
+std::ostream &printTree(std::ostream &out, tree<val, cmp> const tree, bool right, std::string const &prefix)
+{
+    if(tree != nullptr)
+    {
+        out << prefix << (right ? "└──" : "├──" ) << tree->value << "(" << tree->height << ")" << std::endl;
+        printTree(out, tree->right, false, prefix + (right ? "    " : "│   "));
+        printTree(out, tree->left, true, prefix + (right ? "    " : "│   "));
+    }
+    return out;
+}
+
+template<typename val, typename cmp>
+std::ostream &operator<<(std::ostream &out, tree<val, cmp> const tree)
+{
+    return printTree(out, tree, true, "");
+}
